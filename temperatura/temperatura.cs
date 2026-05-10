5f429d23875ba8637af36b7ff2711ffa00131bc0
Piotr <Piotr@SNAKE>
2026-05-10 14:01:38 +0200

    Add project files.

---
+++ temperatura/temperatura.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Wybierz kierunek konwersji (C/F): ");
+        string wybor = Console.ReadLine().ToUpper();
+
+        Console.Write("Podaj temperaturę: ");
+        double temp = Convert.ToDouble(Console.ReadLine());
+
+        double wynik;
+
+        if (wybor == "C")
+        {
+            wynik = (temp * 9 / 5) + 32;
+            Console.WriteLine(temp + "°C = " + wynik + "°F");
+        }
+        else if (wybor == "F")
+        {
+            wynik = (temp - 32) / 1.8;
+            Console.WriteLine(temp + "°F = " + wynik + "°C");
+        }
+        else
+        {
+            Console.WriteLine("Nieprawidłowy wybór!");
+        }
+    }
+}
